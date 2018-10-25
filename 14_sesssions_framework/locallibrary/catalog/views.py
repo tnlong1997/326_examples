0fa287e520f6a830a00b24597f2f16b088bc66dc
Tim Richards <tim.d.richards@gmail.com>
2018-10-25 08:18:42 -0400

    14 update

---
+++ 14_sesssions_framework/locallibrary/catalog/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from catalog.models import Book, Author, BookInstance
+from django.views import generic
+
+
+# Create your views here.
+def index(request):
+    """View function for home page of site."""
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(
+        status__exact="a").count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    # Additions
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_books": num_books,
+        "num_instances": num_instances,
+        "num_instances_available": num_instances_available,
+        "num_authors": num_authors,
+        "num_visits": num_visits,  # Addition
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, "index.html", context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    template_name = "book_list.html"
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = "book_detail.html"
